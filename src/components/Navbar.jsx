d1a41160296813ed27a90eca402a482b6af1690d
Teetras <tanya.prokhorenko.2017@Gmail.com>
2025-02-05 22:06:42 +0300

    fixed some style of navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
 //rsc->
 import { appleImg, searchImg, bagImg } from "../utils/index";
 import { navLists } from "../contants/index";
+import "../assets/styles/carusel.css";
 
 const Navbar = () => {
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center padding">
-      <nav className="flex w-full header-screen">
-        <img src={appleImg} alt="Apple" width={14} height={18} />
+      <nav className="flex flex-wrap w-full header-screen ">
+        <img
+          src={appleImg}
+          alt="Apple"
+          width={14}
+          height={18}
+          className="mr-20 "
+        />
 
         <div className="flex flex-1 justify-center max-sm:hidden gap-7">
           {navLists.map((nav) => (
